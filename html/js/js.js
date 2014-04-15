2152d232fd8184e3d1c378ec1ba6388dfa0ce288
shnapsus <shnapsus@gmail.com>
2014-04-15 18:02:10 +0300

    222

--- html/js/js.js
+++ html/js/js.js
@@ -1,6 +1,43 @@
 $(document).ready(function(){
 
 
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+				title: "Bubble",
+				m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+				oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+			});
+		},
+		swfPath: "/js",
+		supplied: "m4a, oga"
+	});
+
+	$('.show__more').click(function () {
+		if($(this).find('em').text()=='Подробнее'){
+			$(this).parent().addClass('open');
+			$(this).find('em').text('Свернуть')
+		} else {
+			$(this).parent().removeClass('open');
+			$(this).find('em').text('Подробнее')
+		}
+
+		return false;
+	});
+
+	$('.photo__item').click(function () {
+		$('.photo__row__h').css('display', 'none');
+		$('.photo__row'+ $(this).attr('href')).css('display', 'block');
+
+		return false;
+	});
+
+	if($('.photo__item2').length>0){
+		$(".photo__item2").fancybox({
+			openEffect	: 'none',
+			closeEffect	: 'none'
+		});
+	}
 
 	jQuery('input[placeholder], textarea[placeholder]').placeholder();
 });
